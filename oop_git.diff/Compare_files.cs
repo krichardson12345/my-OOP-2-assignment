41d749492c77a5eee8ff37613944cc4fcc3577f6
krichardson12345 <k.richardson12345@gmail.com>
2020-03-29 14:01:20 +0100

    finished assignment
    all done :D

---
+++ oop_git.diff/Compare_files.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace compare_files_assignment
+{
+    class Compare_files
+    {
+        public static bool Compare()
+        {
+            //reads in two files
+            string[] file_1 = File.ReadAllLines(@"file1.txt");
+            Console.WriteLine(file_1[0]);
+            string[] file_2 = File.ReadAllLines(@"file2.txt");
+            Console.WriteLine(file_2[0]);
+
+
+            int i;
+            //tests the two string arrays against each other and returns a bool if they are/arent
+            if (file_1.Length != file_2.Length)
+            {
+                return false;
+            }
+            else
+            {
+                for (i = 0; i < file_1.Length; i++)
+                {
+                    if (file_1[i] != file_2[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ oop_git.diff/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace compare_files_assignment
+{
+    class Program
+    {
+       public static void Main()
+        {
+            bool x = Compare_files.Compare();
+            //program uses compaes bool value to output answer to user
+            if (x == true)
+            {
+                Console.WriteLine("the two strings are equal");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("the two files are not equal");
+                Console.ReadLine();
+            }
+            Console.WriteLine("please press enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
